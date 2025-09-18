a300ddb84b4446e4c729bc9de0e19e983b676a12
Erkan3034 <turguterkan55@gmail.com>
2025-09-18 23:47:41 +0300

    Update API request in deneme2.py to fetch data from a specific GitHub user and ensure proper response handling

--- feature/deneme2.py
+++ feature/deneme2.py
@@ -1,9 +1,10 @@
 import requests
 
-response = requests.get("https://api.github.com/")
+response = requests.get("https://github.com/Erkan3034")
 
 print(f"STATUS CODE: {response.status_code}")
 #print(f"RESPONSE: {response.json()}")
 
 for i in response.json():
     print(i)
+
